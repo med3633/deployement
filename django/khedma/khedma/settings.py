1f47d08da42868b7eb83e4aa17a0d7f222032f72
med363 <blibechmedamine@gmail.com>
2024-04-17 01:35:38 -0700

    update arch

--- django/khedma/khedma/settings.py
+++ django/khedma/khedma/settings.py
@@ -108,7 +108,8 @@ ROOT_URLCONF = 'khedma.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'build')],
+        # 'DIRS': [os.path.join(BASE_DIR,'build')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
