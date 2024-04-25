3ddb2884ef4b1b5e38d6d62051d8519695c09bd8
med363 <blibechmedamine@gmail.com>
2024-04-25 05:27:46 -0700

    ...

--- django/khedma/users/views.py
+++ django/khedma/users/views.py
@@ -344,11 +344,12 @@ class PersonneCreateView(APIView):
                 personne = Personne.objects.create(**personne_data)
 
             # Send activation email
-            current_site = get_current_site(request)
-           
+            # current_site = get_current_site(request)
+            address = "51.255.49.204"
             token = default_token_generator.make_token(user)
             uid = encode_uid(str(user.pk))  # Convert user primary key to UID
-            activation_url = f"{current_site}/activate/{uid}/{token}"
+            # activation_url = f"{current_site}/activate/{uid}/{token}"
+            activation_url = f"{address}/activate/{uid}/{token}"
 
 
             # Build the activation URL
