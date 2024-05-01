8cf6843e68f59cbeae299a20ded3469e44f49a66
med363 <blibechmedamine@gmail.com>
2024-05-01 08:21:51 -0700

    ..

--- django/khedma/khedma/settings.py
+++ django/khedma/khedma/settings.py
@@ -212,7 +212,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
